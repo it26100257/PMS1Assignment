596de1f5d92e0c2acc92e9213e96836601a87ff0
it26100257 <it26100257@my.sliit.lk>
2026-06-25 12:46:28 +0000

    day 2 mehhehhe

--- day1.c
+++ day1.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #define ROWS 20
 #define COLS 40
 #define EMPTY ' '
 #define WALL '#'
-#define TREASURE '$'
-#define BORDER '*'
+#define TREASURE 'T'
+#define HEALTH_PACK 'H'
+#define KEY 'K'
+#define LOCKED_DOOR 'D'
+#define MAX_PLAYERS 3
 
 char map[ROWS][COLS];
 
-// Function prototypes
 void initializeMap();
 void placeWalls();
 void placeTreasures();
+void readKeyboard();
+void move(int new_x, int new_y);
 
 int main() {
-    srand(time(NULL));
     initializeMap();
+    placeWalls();
+    placeTreasures();
 }
 
 void initializeMap() {
     // Zero-fill arrays (set all cells to EMPTY)
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            map[i][j] = EMPTY;
+            
         }
     }
 
-    // Draw border walls
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            if (i == 0 || i == ROWS - 1 || j == 0 || j == COLS - 1) {
-                map[i][j] = BORDER;
-            }
-        }
-    }
+   /////removed here  // Draw border walls
+   // for (int i = 0; i < ROWS; i++) {
+     //   for (int j = 0; j < COLS; j++) {
+       //     if (i == 0 || i == ROWS - 1 || j == 0 || j == COLS - 1) {
+         //       map[i][j] = BORDER;
+         //   }
+     //   }
+    //}
 
     // Call all place* helpers
     placeWalls();
@@ -67,4 +75,21 @@ void placeTreasures() {
             placed++;
         }
     }
+}
+
+void readKeyboard(){
+     char ch;
+    scanf(" %c", &ch);  // Note the space before %c to skip whitespace
+    ch = tolower(ch); 
+
+    switch (ch){
+        case 'w' : move (0, -1); break;
+        case 's' : move (0,  1); break;
+        case 'a' : move (-1, 0); break;
+        case 'd' : move (1,  0); break;
+    }
+}
+
+void move(int new_x, int new_y){
+
 }
