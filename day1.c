fe47412ee46be7b1526f3e3055bf4ac3593e72f8
it26100257 <it26100257@my.sliit.lk>
2026-07-02 20:12:01 +0530

    a couple more functions done

--- day1.c
+++ day1.c
@@ -24,7 +24,7 @@
 #define EMPTY_SYMBOL ' '
 #define MAX_PLAYERS 3
 #define STARTING_HP 100
-#define MAX_HP 100
+#define MAX_HEALTH 100
 #define MAX_MOVES 4
 #define MAX_ENTRIES 100
 #define MSG_LENGTH 256
@@ -42,18 +42,19 @@ void placeKeys();               //done
 void placeDoors();              //done
 void placePlayers();            //done
 void printMap();                //done
-void movePlayer(int index);                   //1
-int  isValidMove(int x, int y);               //2
-void processTile(int index);                  //3
-void gameLoop();                              //4
-void saveGame();                              //5
-void loadGame();                              //6
-void showScores();                            //7
-int  remainingTreasures();                    //8
-void addLog(int r, char *m);                  //9
-void printRecentLog();                        //10
-void saveLog();                               //11
-void showStats();                             //12
+void movePlayer(int index);     //done
+int  isValidMove(int x, int y); //done             
+void processTile(int index);    //done
+int  checkGameEnd();            //done
+void gameLoop();                //done
+void saveGame();                //done
+int  loadGame();                //done
+void showScores();              //started, so eepy
+int  remainingTreasures();      //to do******
+void addLog(int r, char *m);                  //1
+void printRecentLog();                        //2
+void saveLog();                               //3
+void showStats();                             //4
 
 char map[ROWS][COLS], eventLog[MAX_ENTRIES][MSG_LENGTH];
 int  hiddenTrap[ROWS][COLS];
@@ -251,6 +252,245 @@ void printMap(){
         printf("---------------------------------------------\n");
 }
 
+void movePlayer(int index){
+	if(!players[index].alive){
+		return;
+	}	
+		char move, movesMade= 0;
+		printf("%s (%c) - enter 4 moves(WASD):\n", players[index].name, players[index].symbol);
+
+		//get 4 moves
+		for (int i=0; i<MAX_MOVES; i++){
+			printf("move %d: ", i+1);
+			scanf("%c", &move);
+			move= toupper(move);
+
+			int dx=0, dy= 0; //change made in position
+			switch(move){
+				case 'W': dx= -1; 
+					  break;
+				case 'S': dx= 1; 
+				          break;
+				case 'A': dx= -1;
+				          break;
+				case 'D': dx =1;
+					  break;
+				default: 
+					  printf("invalid move '%c' skipped", move);
+					  i--;
+					  continue;
+			}
+			//new position of player.
+			int newX = players[index].x + dx;
+			int newY = players[index].y + dy;
+                        
+			if(!isValidMove(newX, newY)){
+				printf("invalid move!!\n");
+				continue;
+			}
+
+			//locked doors
+			if(map[newX][newY] == DOOR_SYMBOL){
+				if(players[index].keys > 0){
+					players[index].keys--;
+					players[index].doorsUnlocked++;
+					map[newX][newY] = EMPTY_SYMBOL;
+					printf("door unlocked! keys remaining: %d\n", players[index].keys);
+				} else{
+					printf("door locked! you have no keys.");
+					continue;
+				}
+			}
+			//empty tile on previous position
+			map[players[index].x][players[index].y] = EMPTY_SYMBOL;
+			//update player position
+			players[index].x = newX;
+			players[index].y = newY;
+		        players[index].movesMade++;
+		        map[newX][newY] = players[index].symbol;
+		        
+			processTile(index);
+
+			//check if player died after processtile..
+			if (!players[index].alive){
+				map[newX][newY] = EMPTY_SYMBOL;
+				printf("%s has died!", players[index].name);
+				break;
+			}
+
+			movesMade++;
+		}
+
+		if(movesMade == MAX_MOVES){
+			printf("turn complete, 4 moves made");
+		}
+}
+
+int isValidMove(int x, int y){
+	if(x<0 || x>=ROWS){
+		return 0;
+	} else if(y<0 || y>=COLS){
+		return 0;
+	} else if(map[x][y]==WALL_SYMBOL){
+		return 0;
+	} else{
+		return 1;
+	}
+}
+
+void processTile(int index){
+	int x= players[index].x;
+	int y= players[index].y;
+	
+	//check for hidden traps
+	if(hiddenTrap[x][y] ==1){
+		players[index].health-= TRAP_DAMAGE;
+		players[index].trapsTriggered++;
+		players[index].damageTaken+= TRAP_DAMAGE;
+		hiddenTrap[x][y] = 0;
+
+                printf("%s triggered a trap!! new HP: %d\n", players[index].name, players[index].health);
+	
+	        if(players[index].health <= 0){
+		        players[index].health = 0;
+		        players[index].alive = 0;
+		        printf("%s has been eliminated!\n", players[index].name);
+	         }
+		return;
+	}
+
+	//check for treasures
+	if(map[x][y] ==TREASURE_SYMBOL){
+		players[index].score+= TREASURE_SCORE;
+                players[index].treasuresFound++;
+		printf("treasure found! new SCORE:%d\n", players[index].score);
+		return;
+	}
+	if(map[x][y] == HEALTH_SYMBOL){
+                if(players[index].health == MAX_HEALTH){
+			printf("already at maxx health 100!\n");
+		} else{
+			players[index].health+= HEALTH_RESTORE;
+			if(players[index].health> MAX_HEALTH){
+				players[index].health = MAX_HEALTH;
+			}
+			players[index].healthPacksUsed++;
+		
+		        printf("%s health is at %d, %d health packs used.\n", players[index].name, players[index].health, players[index].healthPacksUsed);
+		}
+		return;
+	}
+	if(map[x][y]==KEY_SYMBOL){
+		players[index].keys++;
+                players[index].keysCollected++;
+		printf("%s has collected a key! current key count: %d\n", players[index].name, players[index].keys);
+		return;
+	}
+}
+
+int checkGameEnd(){
+	if(remainingTreasures() ==0){
+		return 1;
+	} 
+	int aliveCount= 0;
+	for(int i=0; i<playerCount; i++){
+		if(players[i].alive){
+			aliveCount++;
+		}
+	}
+	if(aliveCount ==0){
+		return 1;
+	}
+	return 0;
+}
+
+void gameLoop(){
+	char answer;
+
+	while(!checkGameEnd()){
+		printMap();
+
+		for(int i= 0; i<playerCount; i++){
+			if(players[i].alive){
+				movePlayer(i);
+				if(checkGameEnd()){
+					break;
+				}
+			}
+		}
+
+		roundCounter++;
+
+		//save game
+		printf("save game? (Y/N): ");
+		scanf(" %c", &answer);
+		getchar();
+		answer = toupper(answer);
+		if(answer== 'Y'){
+			saveGame();
+			printf("game saved~~");
+		}
+	}
+	//saveLogToFile();
+	//showScores();
+	//showStats();
+}
+
+void saveGame(){
+	FILE *file= fopen(SAVE_FILE, "wb");
+	if (file == NULL){
+		printf("error: could not save game.\n");
+		return;
+	}
+
+	fwrite(&playerCount, sizeof(int), 1, file);
+	fwrite(&roundCounter, sizeof(int), 1, file);
+	fwrite(players, sizeof(player), MAX_PLAYERS, file);
+	fwrite(map, sizeof(char), ROWS*COLS, file);
+	fwrite(hiddenTrap, sizeof(int), ROWS*COLS, file);
+
+	fclose(file);
+	printf("game saved!!\n");
+}
+
+int loadGame(){
+	FILE *file= fopen(SAVE_FILE, "rb");
+	if(file ==NULL){
+		return 0;
+	}
+
+	fread(&playerCount, sizeof(int), 1, file);
+
+	if(playerCount<1 || playerCount>MAX_PLAYERS){
+		fclose(file);
+		return 0;
+	}
+
+	fread(&roundCounter, sizeof(int), 1, file);
+	fread(players, sizeof(player), MAX_PLAYERS, file);
+	fread(map, sizeof(char), ROWS*COLS, file);
+	fread(hiddenTrap, sizeof(int), ROWS*COLS, file);
+
+	fclose(file);
+	return 1;
+}
+void showScores(){
+	printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+	printf("                    GAME OVER                        \n");	
+        printf("                   LEADERBOARD                       \n");
+
+	for(int i=0; i<playerCount; i++){
+		if(players[i].alive){
+		        int new_score = players[i].score + (players[i].health/2);
+			printf("%s final score: %d\n",players[i].name, new_score);
+		}else {
+			printf("%s final score: %d\n",players[i].name, players[i].score);
+		}
+	}
+	//rank
+	//display it
+	//check for tie
+}
 //TEMPORARYYYYYYYYYYYYYYYY
 int remainingTreasures(){
 	return 0;
